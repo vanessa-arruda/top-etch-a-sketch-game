88003d0682173fce8e0e71ec856d63980ad5559a
Vanessa Arruda <adm.vanessasouza@gmail.com>
2023-12-26 14:41:17 +0100

    add reset drawing button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // global variables
 const gridContainer = document.getElementById('grid-container');
+const EraseBtn = document.getElementById('reset-btn');
 
+//create grid-board
 function createGrid(gridSize) {
 
   for (r = 0; r < gridSize; r++){
@@ -15,9 +17,31 @@ function createGrid(gridSize) {
 
 createGrid(16);
 
+//create drawing and drawing rules
 function colorCell() {
+  // document.getElementsByClassName("grid-item").style.backgroundColor = "black";
+  const currentColor = this.style.backgroundColor;
 
-  document.getElementsByClassName("grid-item").style.backgroundColor = "black";
+  if (currentColor === 'black') {
+    this.style.backgroundColor = 'white';
+  } else {
+    this.style.backgroundColor = 'black';
+  }
 }
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(item => {
+  item.addEventListener('click', colorCell);
+});
+
+function resetDrawing() {
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(item => {
+    item.style.backgroundColor = 'white';
+  });
+}
+
+// button to reset drawing
+
+EraseBtn.addEventListener("click", resetDrawing);
 
 
